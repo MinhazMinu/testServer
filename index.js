73c387d3e2fba5d74773a27256453a14eb8517d7
Minhaz <minhaz.taher@gmail.com>
2022-01-09 14:39:53 +0600

    first

---
+++ index.js
@@ -0,0 +1,95 @@
+const { request, response } = require("express");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("node js");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.send(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  console.log(request);
+  const note = notes.find((note) => note.id === id);
+  note ? response.json(note) : response.status(404).end();
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  response.status(204).end();
+});
+
+app.post("/api/notes", (request, response) => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+
+  const note = request.body;
+  note.id = maxId + 1;
+  notes = notes.concat(note);
+  console.log(note);
+  response.json(note);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`server running ${PORT}`);
+});
+
+// const http = require("http");
+
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     date: "2019-05-30T17:30:31.098Z",
+//     important: true,
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only Javascript",
+//     date: "2019-05-30T18:39:34.091Z",
+//     important: false,
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     date: "2019-05-30T19:20:14.298Z",
+//     important: true,
+//   },
+// ];
+
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-Type": "text/plain" });
+//   response.end(JSON.stringify(notes));
+// });
+
+// const PORT = 3001;
+// app.listen(PORT);
+// console.log(`Server running ${PORT}`);
